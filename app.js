b745e954178eaf0290e88c7a42dc844b0a84c563
steff-mat <maties.stefan44@gmail.com>
2022-09-03 02:18:42 +0200

    Fix bugs related to dark/light mode

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 let light = document.getElementById("light-mode");
 let dark = document.getElementById("dark-mode");
 let body = document.getElementById("body");
+let backgroundBoxColor = document.getElementsByClassName("bg-box-color");
+
+let blackishDark = "#151515";
+let blackishLight = "#131313";
+let whiteishLight = "#f0f0f0";
+let whiteishDark = "#e9e9e9";
+
+function check(bgBox) {
+    document.getElementsByClassName(backgroundBoxColor);
+    for (var i = 0; i < backgroundBoxColor.length; i++) {
+        backgroundBoxColor[i].style.backgroundColor = bgBox;
+    }
+}
 
 function colorChange(color) {
     body.style.background = color;
 }
 
 light.addEventListener("click", () => {
-    colorChange("#f0f0f0");
+    colorChange(whiteishLight);
+    check(whiteishDark);
 });
 
 dark.addEventListener("click", () => {
-    colorChange("#141414");
+    colorChange(blackishDark);
+    check(blackishLight);
 });
 
 if (
     window.matchMedia &&
     window.matchMedia("(prefers-color-scheme: dark)").matches
 ) {
-    colorChange("#141414");
+    colorChange(blackishDark);
+    check(blackishLight);
 } else if (
     window.matchMedia !=
     window.matchMedia("(prefers-color-scheme: dark)").matches
 ) {
-    colorChange("#f0f0f0");
+    colorChange(whiteishLight);
+    check(whiteishDark);
 }
